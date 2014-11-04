b074e880c28ea5083ad92d6f8998749c59b56ba3
Naissur <vlad.aituganov@gmail.com>
2014-11-04 17:53:35 +0400

    Handling exceptions in nstdbcreator.py, NSTVKFetcher now connects to VK API

--- nstdbcreator.py
+++ nstdbcreator.py
@@ -2,11 +2,25 @@
 
 from nstdbkeeper import NSTDBKeeper
 from nstvkfetcher import NSTVKFetcher
+import sys
 
 def main():
+	if len(sys.argv) < 4:
+		print "Usage: nstdbcreator <app_id> <login> <password>"
+		quit()
+
 	print "NSTDBCreator by NST team"
-	keeper = NSTDBKeeper()
-	fetcher = NSTVKFetcher()
+	app_id = sys.argv[1]
+	login = sys.argv[2]
+	password = sys.argv[3]
+
+	try:
+		fetcher = NSTVKFetcher(app_id, login, password)
+		keeper = NSTDBKeeper()
+	except Exception as e:
+		print e
+		print "Not initialized, quitting..."
+		quit()
 
 	(person_id, frient_list) = fetcher.get_next()
 	keeper.insert(person_id, frient_list)
--- nstvkfetcher.py
+++ nstvkfetcher.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python -t
 
+import vk
+import requests
+
 class NSTVKFetcher:
 
-	""" Fetches list of friends' id's from VK and has internal id counter.
+	""" Fetches list of friends' id's from VK by reading a batch of 25 lists when necessaty and giving it out one by one.
 		Written by NST team.
 	"""
-	_id = 0
+	_batch = []
+	_vkapi = None
 
-	def __init__(self):
-		self._id = 0
+	def __init__(self, app_id, login, password):
+		_batch = []
 		print "NSTVKFetcher: NSTVKFetcher initialized";
+		try:
+			self._vkapi = vk.API(str(app_id), str(login), str(password))
+		except Exception as e:
+			raise e
+			print "NSTVKFetcher: Could not establish connection."
+			print type(e)
 
 	def get_next(self):
-		self._id += 1
+		if not (self._batch) :
+			print "NSTVKFetcher: Creating new batch..."
+			self._batch = [(1, [2,3,4,5]), (2, [4,5,6,7]), (3, [10,12,14,15])]
 
+		print "NSTVKFetcher: Batch has "+str(len(self._batch))+" entities, popping"
 
-		res_id = self._id
-		print "NSTVKFetcher: Returning "+str(self._id)+"'s friends list..."
-		res_list = [self._id]
-		return (res_id, res_list)
+		next_tuple = self._batch.pop()
+		print "NSTVKFetcher: Returning "+str(next_tuple[0])+"'s friends list"
+		return next_tuple
